6896a84e74927cf4d69e5cd9d5a5e1eda39f7db7
Thiago099 <Thiago099@users.noreply.github.com>
2025-08-18 22:13:40 -0300

    Fixed the item charge verification and refactored nested loops into linear structure

--- src/AttackHandleService.cpp
+++ src/AttackHandleService.cpp
@@ -4,39 +4,49 @@
 
 void AttackHandleService::ResetTime() { lastActivatedTime = 0; }
 
+// TODO: Refactor this code
 void AttackHandleService::AttackDetected(RE::Character* attacked, RE::TESObjectREFR* attackerReference) {
-    if (auto attackerActor = attackerReference->As<RE::Actor>()) {
-        if (attackerActor->IsPowerAttacking()) {
-            auto weapon = attackerActor->GetAttackingWeapon();
-            if (weapon) {
-                if (weapon->GetEnchantmentCharge() > 0) {
-                    if (auto obj = weapon->GetObject()) {
-                        if (obj->HasKeywordByEditorID("QTRLightningfangKeyword")) {
-                            const std::lock_guard<std::mutex> lock(mtx);
-                            auto now = RE::Calendar::GetSingleton()->GetHoursPassed();
-                            if ((now - lastActivatedTime) > 0.005) {
-                                lastActivatedTime = now;
-                                if (auto caster =
-                                        attackerActor->GetMagicCaster(attackerActor->GetEquippedObject(true) == obj?RE::MagicSystem::CastingSource::kLeftHand:RE::MagicSystem::CastingSource::kRightHand)) {
-                                    caster->CastSpellImmediate(
-                                        RE::TESForm::LookupByEditorID<RE::MagicItem>("QTRChainLightning"), false,
-                                        attacked, 1.0f, true, 1, attackerActor);
+    auto attackerActor = attackerReference->As<RE::Actor>();
+    if (!attackerActor) return;
+    auto attackerActorValue = attackerActor->AsActorValueOwner();
+    if (!attackerActorValue) return;
+    auto weapon = attackerActor->GetAttackingWeapon();
+    if (!weapon) return;
+    auto enchantment = weapon->GetEnchantment();
+    if (!enchantment) return;
+    auto obj = weapon->GetObject();
+    if (!obj) return;
+    
+    auto cost = enchantment->CalculateMagickaCost(attackerActor);
 
-                                    auto fx = RE::TESForm::LookupByID<RE::TESBoundObject>(0x2064A);
-                                    auto position = attacked->GetPosition() + RE::NiPoint3{0, 0, 80};
-                                    auto angle = attacked->GetAngle();
-                                    auto cell = attacked->GetParentCell();
-                                    auto worldspace = attacked->GetWorldspace();
-                                    RE::TESDataHandler::GetSingleton()->CreateReferenceAtLocation(
-                                        fx, position, angle, cell, worldspace, nullptr, nullptr, RE::ObjectRefHandle(),
-                                        false, true);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    auto equipedLeft = attackerActor->GetEquippedObject(true);
+    auto equipedRight = attackerActor->GetEquippedObject(false);
+    auto isLeftWeapon = equipedLeft == obj;
+    auto charge = isLeftWeapon
+        ? attackerActorValue->GetActorValue(RE::ActorValue::kLeftItemCharge)
+        : attackerActorValue->GetActorValue(RE::ActorValue::kRightItemCharge);
+    auto now = RE::Calendar::GetSingleton()->GetHoursPassed();
+    auto caster = attackerActor->GetMagicCaster(isLeftWeapon ? RE::MagicSystem::CastingSource::kLeftHand
+                                                             : RE::MagicSystem::CastingSource::kRightHand);
+    if (!caster) return;
+
+    if (!attackerActor->IsPowerAttacking()) return;
+    if (charge < cost) return; 
+    if (!obj->HasKeywordByEditorID("QTRLightningfangKeyword")) return;
+    if ((now - lastActivatedTime) <= 0.005) return;
+
+    const std::lock_guard<std::mutex> lock(mtx);
+    lastActivatedTime = now;
+    caster->CastSpellImmediate(
+        RE::TESForm::LookupByEditorID<RE::MagicItem>("QTRChainLightning"), false,
+        attacked, 1.0f, true, 1, attackerActor);
+
+    auto fx = RE::TESForm::LookupByID<RE::TESBoundObject>(0x2064A);
+    auto position = attacked->GetPosition() + RE::NiPoint3{0, 0, 80};
+    auto angle = attacked->GetAngle();
+    auto cell = attacked->GetParentCell();
+    auto worldspace = attacked->GetWorldspace();
+    RE::TESDataHandler::GetSingleton()->CreateReferenceAtLocation(
+        fx, position, angle, cell, worldspace, nullptr, nullptr,
+        RE::ObjectRefHandle(), false, true);
 }
-
